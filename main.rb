f3eb6f0224c8b8d7834612ae53e8198752287b2b
Evelyn Bauer <evybauer@gmail.com>
2020-02-13 17:08:44 +0000

    Add players to the game

--- main.rb
+++ main.rb
@@ -1,33 +1,13 @@
-require 'players'
-require 'game'
+require './players.rb'
+require './questions.rb'
 
-puts {initial_message}
+class Game
 
-puts "Player 1 name:"
-player1_name = gets.chomp
+  def initialize
+    @current_player = 'Player 1'
 
-puts "Player 2 name:"
-player2_name = gets.chomp
-
-puts "Hello #{player1_name} and #{player2_name}, welcome to the game!"
-
-#PROMPT
-puts current_player: {question}
-player_answer = gets.chomp
-puts {result_output}
-puts {overall_score}
-
-#
-{new_turn_message}
-puts current_player: {question}
-player_answer = gets.chomp
-puts Player 2: {correct_answer_message} 
-puts {overall_score}
-
-#
-{new_turn_message}
-puts {winner_message}
-
-#
-{last_turn_message}
-puts {game_over_message}
+    @first_player = Player.new
+    @second_player = Player.new
+  end 
+  
+end
