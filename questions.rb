cd1d501bda3f5a3f8452512314df25e5e79371c3
Evelyn Bauer <evybauer@gmail.com>
2020-02-13 17:33:47 +0000

    Add current player to initialize method

--- questions.rb
+++ questions.rb
@@ -1,10 +1,10 @@
 class Questions
 
-    def initialize
+    def initialize(current_player)
       @num_1 = rand(20)
       @num_2 = rand(20)
       @answer = @num_1 + @num_2
-      
+
       puts "#{current_player}: What does #{@num_1} plus #{@num_2} equal?"
       ask_question
     end
