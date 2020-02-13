fde5d4c7f02e22c0b1f7028dc28c833f55196b3f
Evelyn Bauer <evybauer@gmail.com>
2020-02-13 17:23:27 +0000

    Add round to the game

--- main.rb
+++ main.rb
@@ -11,7 +11,18 @@ class Game
 
     @player_1_lives = "#{@first_player.lives}/3"
     @player_2_lives = "#{@second_player.lives}/3"
-    
+
   end 
+
+  def round
+    puts '----- NEW TURN -----'
+    #ask a question
+
+    if !ask_question.correct
+    #something
+    end
+
+    #result of round
+    puts "P1: #{@player_1_lives} vs P2: #{@player_2_lives}"
 
 end
