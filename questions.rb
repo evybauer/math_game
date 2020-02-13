3695e32f4c40dd218476cfccf69beced760c45ed
Evelyn Bauer <evybauer@gmail.com>
2020-02-13 17:19:14 +0000

    Add questions output

--- questions.rb
+++ questions.rb
@@ -1,2 +1,19 @@
 class Questions
+
+
+    def ask_question
+
+      @player_answer = gets.chomp
+
+      if @player_answer == @answer.to_s
+        puts "That's right!"
+        @correct = true
+
+      else
+        puts "Nope, that's wrong!"
+        @correct = false
+
+      end
+      
+  end
 end
